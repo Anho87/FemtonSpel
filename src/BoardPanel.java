df66891f9b51063750cca35d7ee5e7389bc1d472
Venus Poon <venuspsw@hotmail.com>
2023-10-29 13:54:31 +0100

    display random buttons on boardpanel

--- src/BoardPanel.java
+++ src/BoardPanel.java
@@ -1,25 +1,33 @@
 import javax.swing.*;
 import java.awt.*;
+import java.time.temporal.JulianFields;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.random.RandomGenerator;
 
 public class BoardPanel extends JPanel {
-    ArrayList<JButton> buttonlist;
+
+    ArrayList<JButton> buttonlist = new ArrayList<>();
 
     public ArrayList<JButton> createRandomButtonList(){
         ArrayList<JButton> temp = new ArrayList<>();
         temp.add(new JButton(" "));
         for (int i = 1; i < 16; i++) {
-            buttonlist.add(new JButton(String.valueOf(i)));
+            temp.add(new JButton(String.valueOf(i)));
         }
         Collections.shuffle(temp);
-        return temp;
+        return new ArrayList<>(temp);
     }
 
     BoardPanel(){
+        //create a random button list
         buttonlist = createRandomButtonList();
 
         setLayout(new GridLayout(4,4));
 
+        //display buttons on boardpanel
+        for (JButton i : buttonlist){
+            add(i);
+        }
     }
 }
