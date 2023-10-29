19bf6d4eb2d8fe4fad2986cccfe0571c9d7ff433
Anho87 <145834751+Anho87@users.noreply.github.com>
2023-10-29 09:37:06 +0100

    la till 2 paneler

--- src/FemtonSpel.java
+++ src/FemtonSpel.java
@@ -7,9 +7,15 @@ public class FemtonSpel extends JFrame implements ActionListener {
     public static void main(String[] args) {
         new FemtonSpel();
     }
+    
+    MenuPanel menuPanel = new MenuPanel();
+    BoardPanel boardPanel = new BoardPanel();
     FemtonSpel(){
         setTitle("FemtonSpel");
         setLayout(new BorderLayout());
+        
+        add(menuPanel, BorderLayout.NORTH);
+        add(boardPanel, BorderLayout.CENTER);
         
         
         
---
+++ src/MenuPanel.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class MenuPanel extends JPanel {
+    
+    JButton newGame = new JButton("New Game");
+    MenuPanel(){
+        add(newGame);
+    }
+}
