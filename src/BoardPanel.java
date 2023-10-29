4371787e8f054e712e8a9d329ba27e502b553759
Venus Poon <venuspsw@hotmail.com>
2023-10-29 13:25:25 +0100

    create a random button list.

--- src/BoardPanel.java
+++ src/BoardPanel.java
@@ -1,8 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class BoardPanel extends JPanel {
+    ArrayList<JButton> buttonlist;
+
+    public ArrayList<JButton> createRandomButtonList(){
+        ArrayList<JButton> temp = new ArrayList<>();
+        temp.add(new JButton(" "));
+        for (int i = 1; i < 16; i++) {
+            buttonlist.add(new JButton(String.valueOf(i)));
+        }
+        Collections.shuffle(temp);
+        return temp;
+    }
+
     BoardPanel(){
+        buttonlist = createRandomButtonList();
+
         setLayout(new GridLayout(4,4));
+
     }
 }
