dcb02456f2941f0a26dd91ec4403dce3a2a51e99
Anho87 <145834751+Anho87@users.noreply.github.com>
2023-10-29 19:59:48 +0100

    la till exit knapp

--- src/MenuPanel.java
+++ src/MenuPanel.java
@@ -1,9 +1,21 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MenuPanel extends JPanel {
+public class MenuPanel extends JPanel implements ActionListener {
     
     JButton newGame = new JButton("New Game");
+    JButton exitButton = new JButton("Exit");
     MenuPanel(){
         add(newGame);
+        add(exitButton);
+        exitButton.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == exitButton){
+            System.exit(0);
+        }
     }
 }
