125400c9ea1a1791bac38b4e27b4ba1df8eb4016
Venus Poon <venuspsw@hotmail.com>
2023-10-31 21:20:54 +0100

    simplify exitbutton to FemtonSpel.java by relocation

--- src/FemtonSpel.java
+++ src/FemtonSpel.java
@@ -6,30 +6,34 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class FemtonSpel extends JFrame implements ActionListener {
+public class FemtonSpel extends JFrame {
     static boolean isTest = true;
-    static MenuPanel menuPanel = new MenuPanel();
-    static BoardPanel boardPanel = isTest ? new BoardPanel(isTestMode()) : new BoardPanel();
+//    static MenuPanel menuPanel = new MenuPanel();
+    JPanel menuPanel = new JPanel();
+    BoardPanel boardPanel = isTest ? new BoardPanel(isTestMode()) : new BoardPanel();
+
+    JButton newGame = new JButton("New Game");
+    JButton exitButton = new JButton("Exit");
 
     //custom button array list for test mode
     public static ArrayList<JButton> isTestMode(){
-//        //Case 1: space at TOP-LEFT corner
-//        ArrayList<JButton> buttonListForTest = new ArrayList<>(Arrays.asList(
-//                new JButton("1"), new JButton("5"), new JButton("2"), new JButton("3"),
-//                new JButton("4"), new JButton("16"), new JButton("6"), new JButton("7"),
-//                new JButton("8"), new JButton("9"), new JButton("10"), new JButton("11"),
-//                new JButton("12"), new JButton("13"), new JButton("14"), new JButton("15")
-//        ));
-//        buttonListForTest.get(5).setVisible(false); //button of value "16" => empty button
-
-        //Case 2: space at BOTTOM-RIGHT corner
+        //Case 1: space at TOP-LEFT corner
         ArrayList<JButton> buttonListForTest = new ArrayList<>(Arrays.asList(
-                new JButton("1"), new JButton("2"), new JButton("3"), new JButton("4"),
-                new JButton("5"), new JButton("6"), new JButton("7"), new JButton("8"),
-                new JButton("9"), new JButton("10"), new JButton("16"), new JButton("12"),
-                new JButton("13"), new JButton("14"), new JButton("11"), new JButton("15")
+                new JButton("1"), new JButton("5"), new JButton("2"), new JButton("3"),
+                new JButton("4"), new JButton("16"), new JButton("6"), new JButton("7"),
+                new JButton("8"), new JButton("9"), new JButton("10"), new JButton("11"),
+                new JButton("12"), new JButton("13"), new JButton("14"), new JButton("15")
         ));
-        buttonListForTest.get(10).setVisible(false); //button of value "16" => empty button
+        buttonListForTest.get(5).setVisible(false); //button of value "16" => empty button
+
+//        //Case 2: space at BOTTOM-RIGHT corner
+//        ArrayList<JButton> buttonListForTest = new ArrayList<>(Arrays.asList(
+//                new JButton("1"), new JButton("2"), new JButton("3"), new JButton("4"),
+//                new JButton("5"), new JButton("6"), new JButton("7"), new JButton("8"),
+//                new JButton("9"), new JButton("10"), new JButton("16"), new JButton("12"),
+//                new JButton("13"), new JButton("14"), new JButton("11"), new JButton("15")
+//        ));
+//        buttonListForTest.get(10).setVisible(false); //button of value "16" => empty button
 
         return buttonListForTest;
     }
@@ -41,24 +45,23 @@ public class FemtonSpel extends JFrame implements ActionListener {
     FemtonSpel(){
         setTitle("FemtonSpel");
         setLayout(new BorderLayout());
-        
-        add(menuPanel, BorderLayout.NORTH);
 
         add(boardPanel, BorderLayout.CENTER);
-        menuPanel.newGame.addActionListener(this);
         
+        add(menuPanel, BorderLayout.NORTH);
+        menuPanel.add(newGame);
+        menuPanel.add(exitButton);
+        newGame.addActionListener(e -> {
+            boardPanel.removeAll();
+            boardPanel.buttonArray = boardPanel.shuffleGame();
+            boardPanel.revalidate();
+        });
+        exitButton.addActionListener(e -> {System.exit(0);});
+
         pack();
         setResizable(false);
         setLocationRelativeTo(null);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {if (e.getSource() == menuPanel.newGame){
-            boardPanel.removeAll();
-            boardPanel.buttonArray = boardPanel.shuffleGame();
-            boardPanel.revalidate();
-        }
     }
 }
--- src/MenuPanel.java
+++ src/MenuPanel.java
@@ -1,21 +1,37 @@
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-public class MenuPanel extends JPanel implements ActionListener{
-    
-    JButton newGame = new JButton("New Game");
-    JButton exitButton = new JButton("Exit");
-    MenuPanel(){
-        add(newGame);
-        add(exitButton);
-        exitButton.addActionListener(this);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == exitButton){
-            System.exit(0);
-        }
-    }
-}
+//import javax.swing.*;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//
+//public class MenuPanel extends JPanel{
+//
+//    BoardPanel boardPanel = new BoardPanel();
+//    JButton newGame = new JButton("New Game");
+//    JButton exitButton = new JButton("Exit");
+//    MenuPanel(){
+//        add(newGame);
+//        add(exitButton);
+//        newGame.addActionListener(new startNewGame());
+//        exitButton.addActionListener(new exitGame());
+//    }
+//
+//    class startNewGame implements ActionListener{
+//        @Override
+//        public void actionPerformed(ActionEvent e) {
+//            if (e.getSource() == newGame){
+//                boardPanel.removeAll();
+//                boardPanel.buttonArray = boardPanel.shuffleGame();
+//                boardPanel.revalidate();
+//            }
+//
+//        }
+//    }
+//
+//    class exitGame implements ActionListener{
+//        @Override
+//        public void actionPerformed(ActionEvent e) {
+//            if(e.getSource() == exitButton){
+//                System.exit(0);
+//            }
+//        }
+//    }
+//}
